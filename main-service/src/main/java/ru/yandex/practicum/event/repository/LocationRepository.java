af89e86f9ca1a7b06c7397835b14e92a9d63a1f2
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-04-18 00:30:59 +0300

    2 2 2

--- main-service/src/main/java/ru/yandex/practicum/event/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.event.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.event.model.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
---
+++ stats-service/stats-dto/src/main/java/ru/practicum/client/models/HitDto.java
@@ -0,0 +1,28 @@
+package ru.practicum.client.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class HitDto {
+    private Long id;
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private String ip;
+    @PastOrPresent
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
